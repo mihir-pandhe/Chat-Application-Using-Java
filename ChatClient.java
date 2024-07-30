cb4a22ca6ebcfffdd315f2b0a68dfd748943f84f
Harsh Pandhe <pandhemihir@gmail.com>
2024-07-31 00:08:27 +0530

    Done Some updates.

--- ChatClient.java
+++ ChatClient.java
@@ -5,6 +5,7 @@ public class ChatClient {
     public static void main(String[] args) {
         String serverAddress = "localhost";
         int port = 12345;
+
         try (Socket socket = new Socket(serverAddress, port)) {
             System.out.println("Connected to server.");
 
@@ -12,18 +13,26 @@ public class ChatClient {
             PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
             BufferedReader userInput = new BufferedReader(new InputStreamReader(System.in));
 
+            System.out.print("Enter your username: ");
+            String username = userInput.readLine();
+            out.println(username);
+
             String messageToServer;
             String messageFromServer;
 
             while (true) {
-                System.out.print("Client: ");
-                messageToServer = userInput.readLine();
-                out.println(messageToServer);
-
                 messageFromServer = in.readLine();
                 if (messageFromServer != null) {
                     System.out.println("Server: " + messageFromServer);
                 }
+
+                System.out.print("Client: ");
+                messageToServer = userInput.readLine();
+                if (messageToServer.equalsIgnoreCase("/exit")) {
+                    out.println("/exit");
+                    break;
+                }
+                out.println(messageToServer);
             }
 
         } catch (IOException e) {
--- ChatServer.java
+++ ChatServer.java
@@ -1,5 +1,7 @@
 import java.io.*;
 import java.net.*;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class ChatServer {
     public static void main(String[] args) {
@@ -13,11 +15,22 @@ public class ChatServer {
             PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
             BufferedReader userInput = new BufferedReader(new InputStreamReader(System.in));
 
+            out.println("Enter your username:");
+            String username = in.readLine();
+            System.out.println(username + " has joined the chat.");
+
             String messageFromClient;
             String messageToClient;
 
+            SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
+
             while ((messageFromClient = in.readLine()) != null) {
-                System.out.println("Client: " + messageFromClient);
+                if (messageFromClient.equalsIgnoreCase("/exit")) {
+                    System.out.println("Client has left the chat.");
+                    break;
+                }
+
+                System.out.println("[" + sdf.format(new Date()) + "] " + username + ": " + messageFromClient);
 
                 System.out.print("Server: ");
                 messageToClient = userInput.readLine();
